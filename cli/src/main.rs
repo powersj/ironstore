9043bbc04192d4d3720c644214e2af9846586f56
Josh Powers <powersj@fastmail.com>
2024-03-03 16:39:36 -0700

    feat(cli): Read from file or interactive CLI

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,5 +1,5 @@
 use tokio::net::TcpStream;
-use tokio::io::{self, AsyncWriteExt, AsyncReadExt};
+use tokio::io::{self, AsyncWriteExt, AsyncReadExt, BufReader, AsyncBufReadExt};
 use clap::{Arg, Command, ArgAction};
 
 #[tokio::main]
@@ -9,9 +9,16 @@ async fn main() -> io::Result<()> {
         .author("Joshua Powers <powersj@fastmail.com>")
         .about("ironstore CLI client for sending messages to the server")
         .arg(Arg::new("port")
-             .long("port")
-             .help("The port to connect to")
-             .required(true))
+            .long("port")
+            .help("The port to connect to")
+            .default_value("8080")
+            .required(false)
+        )
+        .arg(Arg::new("file")
+            .long("file")
+            .help("Read commands rom a file")
+            .required(false)
+        )
         .arg(Arg::new("COMMAND").action(ArgAction::Append))
         .get_matches();
 
@@ -19,17 +26,50 @@ async fn main() -> io::Result<()> {
     let address = format!("127.0.0.1:{}", port);
     let mut stream = TcpStream::connect(&address).await.expect("Failed to connect to server");
 
-    let command: Vec<&String> = matches.get_many::<String>("COMMAND").unwrap().collect::<Vec<_>>();
-    let message = command.iter().map(|s| s.as_str()).collect::<Vec<_>>().join(" ");
-    stream.write_all(message.as_bytes()).await.expect("Failed to write to stream");
+    if let Some(file_path) = matches.get_one::<String>("file") {
+        send_commands_from_file(&mut stream, file_path).await?;
+    } else if let Some(commands) = matches.get_many::<String>("COMMAND") {
+        let command: Vec<&String> = commands.collect::<Vec<_>>();
+        let message = command.iter().map(|s| s.as_str()).collect::<Vec<_>>().join(" ");
+        send_command(&mut stream, &message).await?;
+    } else {
+        interactive_mode(&mut stream).await?;
+    }
+
+    Ok(())
+}
+
+async fn send_commands_from_file(stream: &mut TcpStream, file_path: &str) -> io::Result<()> {
+    for line in std::fs::read_to_string(file_path).unwrap().lines() {
+        send_command(stream, line).await?;
+    }
+
+    Ok(())
+}
+
+async fn send_command(stream: &mut TcpStream, message: &str) -> io::Result<()> {
+    stream.write_all(message.as_bytes()).await?;
 
     let mut buffer = [0; 1024];
-    let n = stream.read(&mut buffer).await.expect("Failed to read from stream");
+    let n = stream.read(&mut buffer).await?;
     if n > 0 {
         let response = std::str::from_utf8(&buffer[..n]).expect("Failed to parse response as UTF-8");
-        println!("{}", response);
-    } else {
-        println!("No response received");
+        println!("> {}", response);
+    }
+    Ok(())
+}
+
+async fn interactive_mode(stream: &mut TcpStream) -> io::Result<()> {
+    println!("Enter commands to send to the server. Type 'exit' to quit.");
+
+    let stdin = io::stdin();
+    let mut lines = BufReader::new(stdin).lines();
+    while let Some(line) = lines.next_line().await? {
+        if line.to_lowercase() == "exit" {
+            break;
+        }
+
+        send_command(stream, &line).await?;
     }
 
     Ok(())
